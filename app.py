654b05b6c3bdd406cd5820c7ea45c02f4a25a789
Hamid <vakilzas@uww.edu>
2024-07-31 12:58:18 -0500

    initial commit

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def show_login():
     if verify_api:
         # try the credentials before creating xbrl object
         try_credentials(user_name=username, pass_word=password, client_id=client_id, client_secret=client_secret, store=remember_me)
-        st.experimental_rerun()
+        st.rerun()
 
 
 def input_number_for_integers(key):
